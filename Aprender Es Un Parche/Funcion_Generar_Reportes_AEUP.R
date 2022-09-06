80632b21d9291944250395be1c6e97fe9f0db2a4
SirMotti <82348760+SirMotti@users.noreply.github.com>
2022-09-06 14:20:05 -0500

    Add files via upload

---
+++ Aprender Es Un Parche/Funcion_Generar_Reportes_AEUP.R
@@ -0,0 +1,135 @@
+#Esteban Motta Ruiz - Auxiliar de Porgramación en Ude@
+#Edición de informes de Zoom - 28 de Octubre de 2021
+#Informes de Aprender desde Casa
+#esteban.motta@duea.edu.co
+
+direccion_reg <- "D:/Proyectos/Programacion/RFiles/UdeArroba_Informes_ADC/6Junio/reg_06_16_2022_ADC.csv"
+direccion_par <- "D:/Proyectos/Programacion/RFiles/UdeArroba_Informes_ADC/6Junio/par_06_16_2022_ADC.csv"
+
+nombre_arch <- "D:/Proyectos/Programacion/RFiles/UdeArroba_Informes_ADC/6Junio/Reporte_06_16_2022_ADC.xlsx"
+
+Editar.Reporte.ADC <- function(d1, d2, nom){
+  
+  dir1 <- read.csv(d1, encoding = "UTF-8")
+  dir2 <- read.csv(d2, encoding = "UTF-8")
+  
+  library(stringr)
+  
+  for (i in 1:length(dir1)) {
+       dir1[i] <- str_replace_all(dir1[, i], " ", " ")
+  }
+  
+  for (i in 1:length(dir2)) {
+       dir2[i] <- str_replace_all(dir2[, i], " ", " ")
+  }
+  
+  asist <- rep(1, times = length(dir1[, 1]))
+  ins <- subset.data.frame(dir1[, c(1, 2, 3, 6, 7, 8)])
+  ins_dt <- cbind(ins, asist)
+  
+  colnom1 <- c("Nombre", "Apellido", "Correo", "Programa", "Dependencia",
+               "Vinculacion", "Asist")
+  colnames(ins_dt) <- colnom1
+  
+  ins_dt$Correo <- tolower(ins_dt$Correo)
+
+  ast <- subset.data.frame(dir2[, c(1, 2)])
+  
+  colnom2 <- c("Nombre", "Correo")
+  colnames(ast) <- colnom2
+  
+  ast_dt <- merge(x = ins_dt,
+                  y = ast,
+                  by = "Correo")
+
+  ins_prog <- aggregate(Asist~Programa,
+                        data = ins_dt,
+                        FUN = sum)
+  ins_dep <- aggregate(Asist~Dependencia,
+                       data = ins_dt,
+                       FUN = sum)
+  ins_vin <- aggregate(Asist~Vinculacion,
+                       data = ins_dt,
+                       FUN = sum)
+  
+  ast_prog <- aggregate(Asist~Programa,
+                        data = ast_dt,
+                        FUN = sum)
+  ast_dep <- aggregate(Asist~Dependencia,
+                       data = ast_dt,
+                       FUN = sum)
+  ast_vin <- aggregate(Asist~Vinculacion,
+                       data = ast_dt,
+                       FUN = sum)
+  
+  ast_dt2 <- ast_dt[, 1:7]
+  colnames(ast_dt2)[2] <- "Nombre"
+  ast_dt2$Asist <- "1"
+  
+  no_ast <- data.frame(Correo = setdiff(ins_dt$Correo, ast_dt2$Correo))
+  no_ast_dt <- merge(x = ins_dt,
+                     y = no_ast,
+                     by.x = "Correo",
+                     by.y = "Correo")
+  
+  no_ast_dt$Asist <- "0"
+  
+  general <- rbind(ast_dt2, no_ast_dt)
+  general <- general[c(7, 2, 3, 1, 4, 5, 6)]
+  general$Asist <- as.numeric(general$Asist)
+
+  library(xlsx)
+  
+  write.xlsx(general,
+             nom,
+             sheetName = "Reporte General",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ins_prog,
+             nom,
+             sheetName = "Ins Programa",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ins_dep,
+             nom,
+             sheetName = "Ins Dependencia",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ins_vin,
+             nom,
+             sheetName = "Ins Vinculacion",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ast_prog,
+             nom,
+             sheetName = "Ast Programa",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ast_dep,
+             nom,
+             sheetName = "Ast Dependencia",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+  
+  write.xlsx(ast_vin,
+             nom,
+             sheetName = "Ast Vinculacion",
+             col.names = TRUE,
+             row.names = FALSE,
+             append = TRUE)
+}
+
+Editar.Reporte.ADC(d1 = direccion_reg,
+                   d2 = direccion_par,
+                   nom = nombre_arch)
